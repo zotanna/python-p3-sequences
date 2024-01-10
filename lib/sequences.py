54f9041a7e5ba38b51ad56d76bdf443165cebb18
denis <denniiskim@gmail.com>
2024-01-10 04:45:54 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+    if length > 0:
+        my_list.append(0)
+    if length >= 2:
+        my_list.append(1)
+        for i in range(2, length):
+            my_list.append(my_list[-1] + my_list[-2])
+
+    print(my_list)
+    return my_list
+
+# Example usage:
+result = print_fibonacci(10)
